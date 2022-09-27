1ca20b547c84e5ce8b3e8759a040945436eb7195
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2022-09-27 16:05:16 +0100

    feat: remove lient code

--- GConge.Web.Client/Models/ApiService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GConge.Web.Client.Models;
-
-public sealed class ApiService
-{
-  public string BaseUrl { get; set; } = default!;
-  public string ApiPath { get; set; } = "/api";
-}
--- GConge.Web.Client/wwwroot/Js/utils.js
+++
@@ -1,8 +0,0 @@
-﻿/**
- * @param {string} modalId
- */
-export function CloseModal(modalId) {
-    var myModalEl = document.getElementById(modalId);
-    var modal = bootstrap?.Modal?.getInstance(myModalEl);
-    modal?.hide()
-}
